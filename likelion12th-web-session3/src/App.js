fa5b7f1f5dcfd2aa30f36d45252be7e36686878a
youminki <dbalsrl7648@naver.com>
2024-04-14 01:41:07 +0900

    install img

--- likelion12th-web-session3/src/App.js
+++ likelion12th-web-session3/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>ㄴ
         <a
           className="App-link"
           href="https://reactjs.org"
