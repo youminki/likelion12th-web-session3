35a18e9652b1266c117c6e018c2242aff5d466f5
youminki <dbalsrl7648@naver.com>
2024-04-27 12:44:28 +0900

    pr 수정사항 : 파일나누고 app.js import완료

---
+++ likelion12th-web-session3/src/JsPage/education.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import githubLogo from '../img/github_log.jpeg';
+// import tistoryLogo from '../img/Tistory_logo.png';
+import '../App.css';
+import '../App.js';
+
+function education() {
+  return (
+    <div class="resume">
+    <img src="../img/명지대로고.jpg" width="50%" />
+    <p><strong>현재 재학중(3학년)</strong></p>
+  </div>
+  );
+}
+
+export default education;
