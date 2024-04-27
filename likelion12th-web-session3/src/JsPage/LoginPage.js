5001339e8bd8fd90710a859f3c54d2b3eb646ccb
youminki <dbalsrl7648@naver.com>
2024-04-27 15:58:38 +0900

    add. login page ustate $ click event handler

--- likelion12th-web-session3/src/JsPage/LoginPage.js
+++ likelion12th-web-session3/src/JsPage/LoginPage.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import { default as React, default as React, useState } from 'react';
 import '../App.css';
 import '../App.js';
+import "./Login.css";
 
 export const UserInfo = {
     id: "dbalsrl",
     password: "7648",
 };
 
-import React, { useState } from "react";
-import "./Login.css";
-
-const [Email, setEmail] = useState("");
-const [Password, setPassword] = useState("");
-출처: https://12716.tistory.com/entry/ReactJS-로그인-페이지-구현하기 [공부하는 개발자 강씨네:티스토리]
 
 const LoginPage = () => {
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
-
+    const EmailHandler = (event) => {
+        setId(event.currentTarget.value);
+    }
+    const PasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
     return (
         <div className="wrapper_box">
             <h2>LOG IN</h2>
             <form className="login_form">
                 <input type="text" className="login_input" placeholder="ID" />
+                <input type='id' value={id} onChange={EmailHandler}/>
+                <input type='password' value={password} onChange={PasswordHandler}/>
                 <input placeholder="Password" type="Password" className="login_input" />
                 <button type="submit" className="login_btn">
                     Login
