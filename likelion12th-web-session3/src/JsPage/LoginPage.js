2061e582907ba22edcfa7da44cc0532bc9612a07
youminki <dbalsrl7648@naver.com>
2024-04-27 16:14:38 +0900

    add. handleSubmit
    로그인이 실패했을 경우
    '로그인에 실패하셨습니다' 문구 출력

--- likelion12th-web-session3/src/JsPage/LoginPage.js
+++ likelion12th-web-session3/src/JsPage/LoginPage.js
@@ -4,29 +4,39 @@ import '../App.css';
 import '../App.js';
 import "../Login.css";
 
+
 export const UserInfo = {
     id: "dbalsrl",
     password: "7648",
 };
 
-
-const LoginPage = () => {
+const LoginPage = ({ onLogin }) => {
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
-    const EmailHandler = (event) => {
+
+    const IdHandler = (event) => {
         setId(event.currentTarget.value);
     }
+
     const PasswordHandler = (event) => {
         setPassword(event.currentTarget.value);
     }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (id === UserInfo.id && password === UserInfo.password) {
+            onLogin(true);
+        } else {
+            alert('로그인에 실패하셨습니다.');
+        }
+    }
+
     return (
         <div className="wrapper_box">
             <h2>LOG IN</h2>
-            <form className="login_form">
-                <input type="text" className="login_input" placeholder="ID" />
-                <input type='id' value={id} onChange={EmailHandler}/>
-                <input type='password' value={password} onChange={PasswordHandler}/>
-                <input placeholder="Password" type="Password" className="login_input" />
+            <form className="login_form" onSubmit={handleSubmit}>
+                <input type="text" className="login_input" placeholder="ID" value={id} onChange={IdHandler}/>
+                <input type="password" className="login_input" placeholder="Password" value={password} onChange={PasswordHandler}/>
                 <button type="submit" className="login_btn">
                     Login
                 </button>
